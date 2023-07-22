caf48af00ff6e9dbbc5452ceece7a71de4ee273f
Aviral Dwivedi <elitaivy@Avirals-MacBook-Pro.local>
2023-07-22 16:36:27 +0530

    remove extra space added

--- src/Components/FormField.js
+++ src/Components/FormField.js
@@ -9,6 +9,24 @@ export default function FormField() {
     setText(uppercasetext)
   }
 
+  const handlelowclick= () =>{
+    // console.log('btn clicked' + Text);
+    let str = Text.split(" ")
+    let lowercasetext = Text.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+    setText(lowercasetext)
+  }
+
+  const copyClipboard = () => {
+    let copytext = document.getElementById("myText");
+    copytext.select();
+    navigator.clipboard.writeText(copytext.value);
+  }
+
+  const removeExtraSpace = () => {
+    let newText = Text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
+
   const handleOnChange = (event)=>{
     // console.log('onchange');
     setText(event.target.value)
@@ -19,12 +37,28 @@ export default function FormField() {
 
 
   return (
+    <div>
     <div className='formfield mt-5'>
         <h2>Place Your Text Here</h2>
-        <textarea className="form-control" rows="8" value={Text} onChange={handleOnChange} placeholder="Another input placeholder"/>  
+        <textarea className="form-control" rows="8" id='myText' value={Text} onChange={handleOnChange} placeholder="Enter Your Text Here"/>  
         <div>
-           <button className="btn btn-primary mt-3" onClick={handleupclick}>Uppercase</button>
+           <button className="btn btn-primary mt-3 mx-2" onClick={handleupclick}>UPPERCASE</button>
+           <button className="btn btn-primary mt-3 mx-2" onClick={handlelowclick}>lowercase</button>
+           <button className="btn btn-primary mt-3 mx-2" onClick={copyClipboard}>Copy Text</button>
+           <button className="btn btn-primary mt-3 mx-2" onClick={removeExtraSpace}>Remove Extra Space</button>
         </div> 
     </div>
+    <div className="counts">
+      <button className="btn btn-primary mt-3 mx-2" >{Text.length} Characters</button>
+      <button className="btn btn-primary mt-3 mx-2" >{Text.split(" ").length} Words</button>
+    </div>
+    <div className="preview">
+      <text>{Text}</text>
+    </div>
+
+    </div>
+    
+    
+    
   )
 }
